d1e8e4945cdedf7f4edfe3ce38f0b2085ee714ef
Ciro Di Paolo <cirodipaolo15@gmail.com>
2024-10-20 16:07:26 -0300

    agregado del atributo estudiante y su metodo get y set

--- src/main/java/Control/InicioSesion/inicioSesionData.java
+++ src/main/java/Control/InicioSesion/inicioSesionData.java
@@ -1,8 +1,13 @@
 package Control.InicioSesion;
 
+import Usuarios.*;
+import static Consultas.consultaArchivo.*;
+import static Path.Path.*;
+
 public final class inicioSesionData {
 
-    private static String legajo;
+    private static String legajo;//Se guarda el legajo de quien ingreso al sistema para futuras operaciones
+    private static Estudiante estudiante;//Se guarda el estudiante que ingreso al sistema para futuras operaciones
 
     public static String getLegajo() {
         return legajo;
@@ -10,5 +15,16 @@ public final class inicioSesionData {
 
     public static void setLegajo(String legajo) {
         inicioSesionData.legajo = legajo;
+
     }
+
+    public static Estudiante getEstudiante() {
+        return estudiante;
+    }
+
+    static void setEstudiante() {//Se obtiene el estudiante que ingreso al sistema
+        estudiante = obtenerEstudiante(fileNameAlumnos, legajo);
+    }
+
+
 }
