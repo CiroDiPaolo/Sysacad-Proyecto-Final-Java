03184edf7a39b53df00be1fca667753cbd1fcf11
Ciro Di Paolo <cirodipaolo15@gmail.com>
2024-10-19 21:12:11 -0300

    programado del control de estadoAcademico

--- src/main/java/Control/Estudiante/estadoAcademicoControl.java
+++ src/main/java/Control/Estudiante/estadoAcademicoControl.java
@@ -1,4 +1,50 @@
 package Control.Estudiante;
 
+import Control.EscenaControl;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.stage.Stage;
+
+import static Path.Path.menuPrincipalAlumnos;
+
 public class estadoAcademicoControl {
+
+    private Stage stage;
+
+    @FXML
+    private Button btnVolver;
+
+    @FXML
+    private TableColumn<?, ?> colAnio;
+
+    @FXML
+    private TableColumn<?, ?> colEstado;
+
+    @FXML
+    private TableColumn<?, ?> colFolio;
+
+    @FXML
+    private TableColumn<?, ?> colMateria;
+
+    @FXML
+    private TableColumn<?, ?> colPlan;
+
+    @FXML
+    private TableColumn<?, ?> colTomo;
+
+    @FXML
+    private Label tctMenuPrincipal;
+
+    @FXML
+    void clickBtnVolver(ActionEvent event) {
+
+        stage = (Stage) btnVolver.getScene().getWindow();
+        EscenaControl escena = new EscenaControl();
+        escena.cambiarEscena(menuPrincipalAlumnos, stage, "Menu Principal");
+
+    }
+
 }
