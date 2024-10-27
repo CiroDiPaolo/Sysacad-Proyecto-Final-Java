63dca81a98b8f37aeb92153c6c579c34f6eb8933
Melisa Carrizo <melisacarrizo5@gmail.com>
2024-10-27 14:38:49 -0300

    modifique actualizar()

--- src/main/java/Modelo/iCRUD.java
+++ src/main/java/Modelo/iCRUD.java
@@ -2,10 +2,11 @@ package Modelo;
 
 import Excepciones.CamposVaciosException;
 import Excepciones.EntidadYaExistente;
+import org.json.JSONObject;
 
 public interface iCRUD {
     boolean crear(String path) throws EntidadYaExistente, CamposVaciosException;
-    boolean actualizar(String path);
+    boolean actualizar(String path, JSONObject jsonObject);
     boolean leer(String path);
     boolean borrar(String path);
 
