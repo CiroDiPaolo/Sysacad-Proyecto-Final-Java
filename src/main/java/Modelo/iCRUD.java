11294f7511f5caad6c3bd24e33d8310976bd32a1
Melisa Carrizo <melisacarrizo5@gmail.com>
2024-10-27 16:06:34 -0300

    agregué excepcion DatosIncorrectosException a crear

--- src/main/java/Modelo/iCRUD.java
+++ src/main/java/Modelo/iCRUD.java
@@ -1,11 +1,12 @@
 package Modelo;
 
 import Excepciones.CamposVaciosException;
+import Excepciones.DatosIncorrectosException;
 import Excepciones.EntidadYaExistente;
 import org.json.JSONObject;
 
 public interface iCRUD {
-    boolean crear(String path) throws EntidadYaExistente, CamposVaciosException;
+    boolean crear(String path) throws EntidadYaExistente, CamposVaciosException, DatosIncorrectosException;
     boolean actualizar(String path, JSONObject jsonObject);
     boolean leer(String path);
     boolean borrar(String path);
