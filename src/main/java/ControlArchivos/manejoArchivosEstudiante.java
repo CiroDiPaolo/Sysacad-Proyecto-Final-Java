2067db1d49ab9b20aec0ad61325eaf0f415d5d86
Melisa Carrizo <melisacarrizo5@gmail.com>
2024-10-27 14:38:57 -0300

    realizando actualizar

--- src/main/java/ControlArchivos/manejoArchivosEstudiante.java
+++ src/main/java/ControlArchivos/manejoArchivosEstudiante.java
@@ -97,5 +97,32 @@ public class manejoArchivosEstudiante {
         return false;
     }
 
+    public static JSONObject retornarEstudiante(String legajo,  String fileName)
+    {
+        JSONArray arreglo = new JSONArray(leerArchivoJSON(fileName));
+
+        boolean encontrado = false;
+
+        int i = 0;
+
+        while(i<arreglo.length() && !encontrado)
+        {
+            JSONObject jsonObject = arreglo.getJSONObject(i);
+
+            if(jsonObject.getString("legajo").equals(legajo))
+            {
+                encontrado = true;
+            }
+
+            i++;
+        }
+        if(encontrado)
+        {
+            return arreglo.getJSONObject((i-1));
+        }
+
+        return null;
+    }
+
 
 }
