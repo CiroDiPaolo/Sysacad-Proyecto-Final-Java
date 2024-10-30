0d58ce5d49a7f73a80106161164fe608bd72ea1c
Melisa Carrizo <melisacarrizo5@gmail.com>
2024-10-30 01:42:06 -0300

    arreglue el menu para que quede coherente

---
+++ src/main/java/Control/Administrador/opcionEditarCarreraAdministradorControl.java
@@ -0,0 +1,55 @@
+package Control.Administrador;
+
+import Control.EscenaControl;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import static Path.Path.*;
+
+public class opcionEditarCarreraAdministradorControl {
+    @FXML
+    private Button btnCarrera;
+
+    @FXML
+    private Button btnMaterias;
+
+    @FXML
+    private Button btnVolver;
+
+    @FXML
+    private Label tctMenuPrincipal;
+
+    private Stage stage;
+
+    private EscenaControl escena = new EscenaControl();
+
+    @FXML
+    void clickBtnCarrera(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickBtnMaterias(ActionEvent event) {
+        escena.cambiarEscena(configurarMateriasAdministrador, stage, "Configurar materias");
+    }
+
+    @FXML
+    void clickBtnVolver(ActionEvent event) {
+        escena.cambiarEscena(busquedaCarreraAdministrador, stage, "Configurar carrera");
+    }
+
+    @FXML
+    protected void initialize() {
+
+        Platform.runLater(() -> {
+
+            stage = (Stage) btnVolver.getScene().getWindow();
+
+        });
+
+    }
+}
