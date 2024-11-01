442d8a12795f5030514571993e606f11aec80059
AnnnngieDB <an10dibiassi@gmail.com>
2024-11-01 14:18:59 -0300

    creacion de clase de control

---
+++ src/main/java/Control/Profesores/crearExcelProfesoresControl.java
@@ -0,0 +1,25 @@
+package Control.Profesores;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.text.Text;
+
+public class crearExcelProfesoresControl {
+
+    @FXML
+    private Button btnSalir;
+
+    @FXML
+    private Label tctMenuPrincipal;
+
+    @FXML
+    private Text txtBienvenida;
+
+    @FXML
+    void clickBtnSalir(ActionEvent event) {
+
+    }
+
+}
