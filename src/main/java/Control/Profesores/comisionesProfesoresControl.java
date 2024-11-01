dd3c40a4aceb1857166938fbf9e66bdca0904a65
AnnnngieDB <an10dibiassi@gmail.com>
2024-11-01 14:18:51 -0300

    creacion de clase de control

---
+++ src/main/java/Control/Profesores/comisionesProfesoresControl.java
@@ -0,0 +1,30 @@
+package Control.Profesores;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.text.Text;
+
+public class comisionesProfesoresControl {
+
+    @FXML
+    private Button btnVolver;
+
+    @FXML
+    private ChoiceBox<?> choiceBoxComisionesProfesores;
+
+    @FXML
+    private Label tctMenuPrincipal;
+
+    @FXML
+    private Text txtBienvenida;
+
+    @FXML
+    void clickBtnVolver(ActionEvent event) {
+
+    }
+
+}
+
