435526b5ea82c74ae7d32236e33e5be87701565c
AnnnngieDB <an10dibiassi@gmail.com>
2024-11-01 14:34:34 -0300

    creé de nuevo una clase que andaba mal no se de que

---
+++ src/main/java/Control/Profesores/mesasProfesoresControl.java
@@ -0,0 +1,26 @@
+package Control.Profesores;
+
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.text.Text;
+
+public class mesasProfesoresControl {
+
+    @FXML
+    private Button btnSalir;
+
+    @FXML
+    private Label tctMenuPrincipal;
+
+    @FXML
+    private Text txtBienvenida;
+
+    @FXML
+    void clickBtnSalir(ActionEvent event) {
+
+    }
+
+}
