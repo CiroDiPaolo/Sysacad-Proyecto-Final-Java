8fb912be07a3bbc849cabfe1e3355dcfd681cff8
AnnnngieDB <an10dibiassi@gmail.com>
2024-11-01 14:51:31 -0300

    agregué cosas a la interfaz de materias de profesor

--- src/main/java/Control/Profesores/materiasProfesorControl.java
+++ src/main/java/Control/Profesores/materiasProfesorControl.java
@@ -1,15 +1,33 @@
 package Control.Profesores;
 
+
+import Control.EscenaControl;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+import static Path.Path.inicioSesion;
 
 public class materiasProfesorControl {
 
+    private Stage stage;
+
     @FXML
-    private Button btnSalir;
+    private TableView<?> tablaMateriasProfesor;
+
+    @FXML
+    private TableColumn<?, ?> uno;
+
+    @FXML
+    private TableColumn<?, ?> dos;
+
+    @FXML
+    private Button btnVolver;
 
     @FXML
     private Label tctMenuPrincipal;
@@ -18,9 +36,14 @@ public class materiasProfesorControl {
     private Text txtBienvenida;
 
     @FXML
-    void clickBtnSalir(ActionEvent event) {
+    void clickBtnVolver(ActionEvent event) {
+
+        stage = (Stage) btnVolver.getScene().getWindow();
+        EscenaControl escena = new EscenaControl();
+        escena.cambiarEscena(inicioSesion, stage, "Inicio de sesion");
 
     }
 
 }
+
 
