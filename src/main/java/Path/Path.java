a7aa87475440f8055dc9213b0988f1b1856d4186
AnnnngieDB <an10dibiassi@gmail.com>
2024-11-01 14:19:42 -0300

    añadi cosas en la clase path

--- src/main/java/Path/Path.java
+++ src/main/java/Path/Path.java
@@ -33,7 +33,10 @@ public final class Path {
     public static final String cambiarContra = "/App/Estudiante/cambiarContra.fxml";
 
     //Profesor
-    public static final String comisionesProfesor = "/App/Profesor/comisionesProfesor.fxml";
+    public static final String comisionesProfesores = "/App/Profesores/comisionesProfesor.fxml";
+    public static final String materiasProfesores = "/App/Profesores/materiasProfesores.fxml";
+    public static final String mesasProfesores = "/App/Profesores/mesasProfesores.fxml";
+    public static final String crearExcelProfesores = "/App/Profesores/crearExcelProfesores.fxml";
 
     //Administrador
     public static final String configurarMateriasAdministrador = "/App/Administrador/configurarMateriasAdministrador.fxml";
