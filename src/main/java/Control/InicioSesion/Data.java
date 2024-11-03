9912e5ed2dc9613e4073f497ff150ab6fc607ab6
Melisa Carrizo <melisacarrizo5@gmail.com>
2024-11-03 19:16:15 -0300

    acomodé codigo

--- src/main/java/Control/InicioSesion/Data.java
+++ src/main/java/Control/InicioSesion/Data.java
@@ -4,7 +4,7 @@ import ControlArchivos.manejoArchivosEstudiante;
 import Modelo.Carrera;
 import Modelo.Materia;
 import Usuarios.*;
-import static Consultas.consultaArchivo.*;
+
 import static Path.Path.*;
 
 public final class Data {
@@ -15,33 +15,29 @@ public final class Data {
     private static Carrera carrera;
     private static Materia materiaData;
 
+    //Getters
     public static Carrera getCarrera(){
         return carrera;
-    }
-
-    public void setCarrera(Carrera carrera){
-        this.carrera = carrera;
     }
 
     public static Materia getMateria(){
         return materiaData;
     }
 
-    public void setMateria(Materia materia){
-        this.materiaData = materia;
-    }
+    public static String getAux() { return aux; }
 
     public static String getLegajo() {
         return legajo;
     }
 
+    public static Estudiante getEstudiante() {
+        return estudiante;
+    }
+
+    //Setters
     public static void setLegajo(String legajo) {
         Data.legajo = legajo;
 
-    }
-
-    public static Estudiante getEstudiante() {
-        return estudiante;
     }
 
     static void setEstudiante(String legajo) {
@@ -51,8 +47,14 @@ public final class Data {
 
     public static void setEstudiante(Estudiante estudiante) { Data.estudiante = estudiante; }
 
-    public static String getAux() { return aux; }
-
     public void setAux(String aux) { this.aux = aux; }
+
+    public void setMateria(Materia materia){
+        this.materiaData = materia;
+    }
+
+    public void setCarrera(Carrera carrera){
+        this.carrera = carrera;
+    }
 
 }
