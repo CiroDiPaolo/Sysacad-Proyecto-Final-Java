4fb0e8985ab115224856fd9da2aef568d54b767e
Ciro Di Paolo <cirodipaolo15@gmail.com>
2024-11-12 17:22:12 -0300

    programado de la clase

--- src/main/java/Control/Profesores/elegirComisionControl.java
+++ src/main/java/Control/Profesores/elegirComisionControl.java
@@ -2,7 +2,11 @@ package Control.Profesores;
 
 import Control.EscenaControl;
 import Control.InicioSesion.Data;
+import ControlArchivos.manejoArchivosCarrera;
+import ControlArchivos.manejoArchivosComisiones;
+import Excepciones.CamposVaciosException;
 import Modelo.Comision;
+import Modelo.Materia;
 import Path.Path;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
@@ -11,9 +15,15 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
+
+import java.time.LocalDate;
 import java.util.HashSet;
+import java.util.Iterator;
 
 public class elegirComisionControl {
+
+    @FXML
+    private Button btnAplicar;
 
     @FXML
     private Button btnVolver;
@@ -35,6 +45,38 @@ public class elegirComisionControl {
     }
 
     @FXML
+    void clickBtnAplicar(ActionEvent event) throws CamposVaciosException {
+        String seleccion = choiceComision.getValue();
+
+        if (seleccion != null) {
+            String[] partes = seleccion.split(" - ");
+
+            String nombreComision = partes[0];
+            HashSet<Comision> comisiones = Data.getProfesor().obtenerComisiones(Path.pathComisiones);
+            comisiones = manejoArchivosComisiones.filtrarComisionesPorNombre(nombreComision, comisiones);
+
+            Iterator<Comision> it = comisiones.iterator();
+
+            while(it.hasNext()){
+                Comision comision = it.next();
+
+                if(comision.getCodigoMateria().equals(partes[2])){
+                    Data data = new Data();
+                    data.setComision(comision);
+                    break;
+                }
+
+            }
+
+            EscenaControl escena = new EscenaControl();
+            escena.cambiarEscena(Path.gestionarComisionProfesor, stage, "Gestionar comision");
+
+        } else {
+            throw new CamposVaciosException("Debe seleccionar una comision");
+        }
+    }
+
+    @FXML
     protected void initialize() {
 
         Platform.runLater(() -> {
@@ -44,7 +86,8 @@ public class elegirComisionControl {
             HashSet<Comision> comisiones = Data.getProfesor().obtenerComisiones(Path.pathComisiones);
 
             for(Comision comision : comisiones){
-                choiceComision.getItems().add(comision.getNombre());
+
+                choiceComision.getItems().add(comision.getNombre() + " - " + manejoArchivosCarrera.obtenerMateria(Path.pathCarreras,comision.getCodigoMateria()).getNombre() + " - " + comision.getCodigoMateria());
             }
 
         });
