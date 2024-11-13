72dfc738d197391cb13afb4b44fea32da232b3d2
Melisa Carrizo <melisacarrizo5@gmail.com>
2024-11-13 18:14:55 -0300

    agregué busqueda de un alumno para ver un alumno

---
+++ src/main/java/Control/Administrador/buscarAlumnoAdministradorControl.java
@@ -0,0 +1,66 @@
+package Control.Administrador;
+
+import Control.EscenaControl;
+import Control.InicioSesion.Data;
+import ControlArchivos.manejoArchivosEstudiante;
+import Excepciones.ArchivoNoEncontrado;
+import Excepciones.excepcionPersonalizada;
+import Usuarios.Estudiante;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import static Path.Path.*;
+
+public class buscarAlumnoAdministradorControl {
+
+    @FXML
+    private Button btnBuscar;
+
+    @FXML
+    private Button btnVolver;
+
+    @FXML
+    private Label tctMenuPrincipal;
+
+    @FXML
+    private TextField txtLegajoAlumno;
+
+    private Stage stage;
+
+    @FXML
+    void clickBtnBuscar(ActionEvent event) {
+        try {
+            if (txtLegajoAlumno.getText().isEmpty()) {
+                throw new ArchivoNoEncontrado("Ingrese un legajo valido");
+            } else {
+                if (Consultas.consultaArchivo.buscarClave(fileNameAlumnos, txtLegajoAlumno.getText(), "legajo")) {
+
+                    Data data = new Data();
+                    data.setEstudiante(Estudiante.JSONObjectAEstudiante(manejoArchivosEstudiante.retornarEstudiante(txtLegajoAlumno.getText(), fileNameAlumnos)));
+                    EscenaControl escena = new EscenaControl();
+                    stage = (Stage) btnVolver.getScene().getWindow();
+                    escena.cambiarEscena(verEstudianteAdministrador, stage, "Ver estudiante");
+
+                } else {
+                    throw new ArchivoNoEncontrado("No existe alumno con ese legajo");
+                }
+            }
+        } catch (ArchivoNoEncontrado e) {
+            e.getMessage();
+        }
+    }
+
+    @FXML
+    void clickBtnVolver(ActionEvent event) {
+
+        EscenaControl escena = new EscenaControl();
+        stage = (Stage) btnVolver.getScene().getWindow();
+        escena.cambiarEscena(configurarAlumnosAdministrador,stage,"Configurar Alumno");
+
+    }
+
+}
