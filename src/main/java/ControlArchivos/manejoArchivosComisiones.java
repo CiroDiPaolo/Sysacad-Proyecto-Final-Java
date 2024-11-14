4b73c54e831fe16b8be7e98a12d157a62975c266
Ciro Di Paolo <cirodipaolo15@gmail.com>
2024-11-14 16:56:01 -0300

    documentacion y creado del metodo actualizarEstudiantesDeUnaComision para actualizar los estudiantes si se cambia el codigo de comision

--- src/main/java/ControlArchivos/manejoArchivosComisiones.java
+++ src/main/java/ControlArchivos/manejoArchivosComisiones.java
@@ -1,5 +1,6 @@
 package ControlArchivos;
 
+import Control.InicioSesion.Data;
 import Excepciones.*;
 import Modelo.Comision;
 import Usuarios.Estudiante;
@@ -13,6 +14,7 @@ import java.util.HashSet;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import static ControlArchivos.manejoArchivos.*;
+import static Path.Path.fileNameAlumnos;
 import static Path.Path.pathComisiones;
 
 public final class manejoArchivosComisiones {
@@ -58,7 +60,7 @@ public final class manejoArchivosComisiones {
     /**
      * Método que carga una comisión a un archivo JSON.
      *
-     * @param path Ruta del archivo JSON donde se cargará la comisión.
+     * @param path     Ruta del archivo JSON donde se cargará la comisión.
      * @param comision Objeto JSON que representa la comisión a cargar.
      * @return true si la comisión se cargó con éxito, false en caso contrario.
      * @throws EntidadYaExistente Si la comisión ya existe en el archivo.
@@ -80,20 +82,17 @@ public final class manejoArchivosComisiones {
             jsonArray = new JSONArray();
         }
 
-        boolean flag= false;
+        boolean flag = false;
 
         int i = 0;
-        while(i<jsonArray.length() && !flag)
-        {
-            if(comision.getString("codigoMateria").equals(jsonArray.getJSONObject(i).getString("codigoMateria")) && comision.getString("nombre").equals(jsonArray.getJSONObject(i).getString("nombre")))
-            {
+        while (i < jsonArray.length() && !flag) {
+            if (comision.getString("codigoMateria").equals(jsonArray.getJSONObject(i).getString("codigoMateria")) && comision.getString("nombre").equals(jsonArray.getJSONObject(i).getString("nombre"))) {
                 flag = true;
             }
             i++;
         }
 
-        if(!flag)
-        {
+        if (!flag) {
             jsonArray.put(comision);
             try (FileWriter file = new FileWriter(path)) {
                 file.write(jsonArray.toString(4));
@@ -101,8 +100,7 @@ public final class manejoArchivosComisiones {
             } catch (IOException | JSONException e) {
                 excepcionPersonalizada.excepcion("Ocurrió un error en el programa. Si el problema persiste, comuníquese con su distribuidor.");
             }
-        }else
-        {
+        } else {
             throw new EntidadYaExistente("El nombre de la comisión ya existe en la materia");
         }
 
@@ -112,7 +110,7 @@ public final class manejoArchivosComisiones {
     /**
      * Método que obtiene todas las comisiones de un archivo JSON para un año y carrera específicos.
      *
-     * @param anio Año de las comisiones a obtener.
+     * @param anio      Año de las comisiones a obtener.
      * @param idCarrera Código de la carrera de las comisiones a obtener.
      * @return Lista de objetos Comision correspondientes al año y carrera especificados.
      */
@@ -132,7 +130,7 @@ public final class manejoArchivosComisiones {
     /**
      * Actualiza una comisión en un archivo JSON.
      *
-     * @param path Ruta del archivo JSON que contiene las comisiones.
+     * @param path     Ruta del archivo JSON que contiene las comisiones.
      * @param comision Objeto JSON que representa los datos actualizados de la comisión.
      * @return true si la comisión fue actualizada con éxito, false en caso contrario.
      */
@@ -186,8 +184,8 @@ public final class manejoArchivosComisiones {
      * Método que retorna una comisión de un archivo JSON.
      *
      * @param filename Nombre del archivo JSON que contiene las comisiones.
-     * @param dato Clave del dato a buscar en el archivo JSON.
-     * @param clave Valor de la clave a buscar en el archivo JSON.
+     * @param dato     Clave del dato a buscar en el archivo JSON.
+     * @param clave    Valor de la clave a buscar en el archivo JSON.
      * @return Objeto Comision correspondiente a la clave dada.
      * @throws CamposVaciosException Si la clave está vacía o es nula.
      */
@@ -223,10 +221,10 @@ public final class manejoArchivosComisiones {
         }
     }
 
-        /**
+    /**
      * Método que retorna una lista de comisiones activas de una materia específica con cupos disponibles.
      *
-     * @param path Ruta del archivo JSON que contiene las comisiones.
+     * @param path          Ruta del archivo JSON que contiene las comisiones.
      * @param codigoMateria Código de la materia para filtrar las comisiones.
      * @return Lista de objetos Comision correspondientes a la materia especificada.
      */
@@ -249,7 +247,7 @@ public final class manejoArchivosComisiones {
         return comisiones;
     }
 
-/**
+    /**
      * Obtiene un conjunto de comisiones para un año específico.
      *
      * @param path Ruta del directorio que contiene los archivos JSON de comisiones.
@@ -301,11 +299,12 @@ public final class manejoArchivosComisiones {
     /**
      * Filtra un conjunto de comisiones por su nombre.
      *
-     * @param nombre Nombre de la comisión a buscar.
+     * @param nombre     Nombre de la comisión a buscar.
      * @param comisiones Conjunto de comisiones a filtrar.
      * @return Conjunto de comisiones que coinciden con el nombre dado.
      */
-    public static HashSet<Comision> filtrarComisionesPorNombre(String nombre, HashSet<Comision> comisiones) {        HashSet<Comision> comisionesFiltradas = new HashSet<>();
+    public static HashSet<Comision> filtrarComisionesPorNombre(String nombre, HashSet<Comision> comisiones) {
+        HashSet<Comision> comisionesFiltradas = new HashSet<>();
         for (Comision comision : comisiones) {
             if (comision.getNombre().equals(nombre)) {
                 comisionesFiltradas.add(comision);
@@ -317,7 +316,7 @@ public final class manejoArchivosComisiones {
     /**
      * Obtiene una lista de estudiantes que pertenecen a una comisión específica.
      *
-     * @param pathAlumnos Ruta del archivo JSON que contiene los datos de los estudiantes.
+     * @param pathAlumnos    Ruta del archivo JSON que contiene los datos de los estudiantes.
      * @param codigoComision Código de la comisión a buscar.
      * @return Lista de objetos Estudiante que pertenecen a la comisión especificada.
      */
@@ -345,32 +344,73 @@ public final class manejoArchivosComisiones {
     }
 
     /**
-    * Obtiene una lista de números de archivos JSON que contienen comisiones para una carrera específica.
-    *
-    * @param path Ruta del directorio que contiene los archivos JSON.
-    * @param codigoCarrera Código de la carrera para filtrar los archivos.
-    * @return Lista de números de archivos correspondientes a la carrera especificada.
-    * @throws IOException Si ocurre un error al acceder al directorio o leer los archivos.
-    */
+     * Obtiene una lista de números de archivos JSON que contienen comisiones para una carrera específica.
+     *
+     * @param path          Ruta del directorio que contiene los archivos JSON.
+     * @param codigoCarrera Código de la carrera para filtrar los archivos.
+     * @return Lista de números de archivos correspondientes a la carrera especificada.
+     * @throws IOException Si ocurre un error al acceder al directorio o leer los archivos.
+     */
     public static ArrayList<Integer> obtenerNumerosDeArchivos(String path, String codigoCarrera) throws IOException {
-    ArrayList<Integer> numeros = new ArrayList<>();
-    try (DirectoryStream<Path> stream = Files.newDirectoryStream(Paths.get(path))) {
-        for (Path entry : stream) {
-            if (Files.isRegularFile(entry) && entry.toString().endsWith(".json")) {
-                String fileName = entry.getFileName().toString();
-                if (fileName.contains(codigoCarrera)) {
-                    String numeroStr = fileName.replaceAll("^COMISIONES_" + codigoCarrera + "_(\\d{4})\\.json$", "$1");
-                    if (!numeroStr.equals(fileName)) {
-                        try {
-                            numeros.add(Integer.parseInt(numeroStr));
-                        } catch (NumberFormatException e) {
-                            e.getMessage();
+        ArrayList<Integer> numeros = new ArrayList<>();
+        try (DirectoryStream<Path> stream = Files.newDirectoryStream(Paths.get(path))) {
+            for (Path entry : stream) {
+                if (Files.isRegularFile(entry) && entry.toString().endsWith(".json")) {
+                    String fileName = entry.getFileName().toString();
+                    if (fileName.contains(codigoCarrera)) {
+                        String numeroStr = fileName.replaceAll("^COMISIONES_" + codigoCarrera + "_(\\d{4})\\.json$", "$1");
+                        if (!numeroStr.equals(fileName)) {
+                            try {
+                                numeros.add(Integer.parseInt(numeroStr));
+                            } catch (NumberFormatException e) {
+                                e.getMessage();
+                            }
                         }
                     }
                 }
             }
         }
+        return numeros;
     }
-    return numeros;
+
+    public static boolean actualizarEstudiantesDeUnaComision(String codigoMateria) {
+
+        String codigoAnterior = Data.getComision().getId();
+
+        String codigoNuevo = Data.getAux3();
+
+        boolean resultado = false;
+
+        ArrayList<Estudiante> estudiantes = obtenerEstudiantesDeUnaComision(fileNameAlumnos, codigoAnterior);
+
+        for (Estudiante estudiante : estudiantes) {
+
+            boolean actualizado = false;
+
+            for (int j = 0; j < estudiante.getMaterias().size(); j++) {
+
+                if (estudiante.getMaterias().get(j).getCodigoComision().equals(codigoAnterior)) {
+
+                    estudiante.getMaterias().get(j).setCodigoComision(codigoNuevo);
+                    System.out.println("Estudiante " + estudiante.getNombre() + " actualizado");
+                    actualizado = true;
+                    resultado = true;
+
+                }
+            }
+
+            if (actualizado) {
+                try {
+
+                    estudiante.actualizar(fileNameAlumnos, estudiante.estudianteAJSONObject());
+
+                } catch (Exception e) {
+                    Excepciones.excepcionPersonalizada.alertaAtencion("Error al actualizar los estudiantes de la comisión");
+                }
+            }
+        }
+
+        return resultado;
     }
+
 }
