c4b6e238aa484bd73b6b00665a02cbb5a03f5605
Melisa Carrizo <melisacarrizo5@gmail.com>
2024-11-14 02:51:40 -0300

    agregue ver avisos

---
+++ src/main/java/Control/Administrador/verAvisosAdministradorControl.java
@@ -0,0 +1,55 @@
+package Control.Administrador;
+
+import Control.EscenaControl;
+import ControlArchivos.manejoArchivosAvisos;
+import Modelo.Avisos;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.stage.Stage;
+
+import java.util.ArrayList;
+
+import static Path.Path.configurarAvisosAdministrador;
+import static Path.Path.pathAvisos;
+
+public class verAvisosAdministradorControl {
+
+    @FXML
+    private Button btnVolver;
+
+    @FXML
+    private Label tctMenuPrincipal;
+
+    @FXML
+    private TextArea txtAreaAvisos;
+
+    private Stage stage;
+
+    private EscenaControl escena = new EscenaControl();
+
+    @FXML
+    public void initialize() {
+        Platform.runLater(() -> {
+            stage = (Stage) btnVolver.getScene().getWindow();
+        });
+
+        ArrayList<Avisos> avisos = new ArrayList<>();
+        avisos = manejoArchivosAvisos.retornarAvisos(pathAvisos);
+        StringBuilder stringAvisos = new StringBuilder();
+        for(Avisos aviso : avisos)
+        {
+            stringAvisos.append("===============================================================================================================================\n").append(aviso.getTitulo()).append("\n").append(aviso.getFecha().toString()).append("\n").append(aviso.getSubtitulo()).append("\n").append(aviso.getDescripcion() + "\n\n" );
+        }
+        txtAreaAvisos.setText(String.valueOf(stringAvisos));
+    }
+
+    @FXML
+    void clickBtnVolver(ActionEvent event) {
+        escena.cambiarEscena(configurarAvisosAdministrador,stage,"Configurar avisos");
+    }
+
+}
